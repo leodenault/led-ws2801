c19a80d18a810c6f38256ae2a7d82909f7f9ab57
Leo Denault <leo.denault@gmail.com>
2018-12-09 02:01:42 -0500

    Improve quality of colours.

--- led_colour.py
+++ led_colour.py
@@ -17,9 +17,9 @@ class Colour:
     else:
       return "({0}, {1}, {2})".format(self.r, self.g, self.b)
 
-Colour.WHITE = Colour(255, 255, 255, "White")
-Colour.RED = Colour(255, 0, 0, "Red")
-Colour.GREEN = Colour(0, 255, 0, "Green")
-Colour.BLUE = Colour(0, 0, 255, "Blue")
+Colour.WHITE = Colour(255, 255, 125, "White")
+Colour.RED = Colour(150, 0, 0, "Red")
+Colour.GREEN = Colour(0, 125, 0, "Green")
+Colour.BLUE = Colour(0, 0, 125, "Blue")
 Colour.BLACK = Colour(0, 0, 0, "Black")
 Colour.GOLD = Colour(255, 125, 0, "Gold")
