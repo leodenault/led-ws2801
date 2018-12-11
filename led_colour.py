97b934dbb567028de5e21f5cc35d7605660416ac
Leo Denault <leo.denault@gmail.com>
2018-12-10 22:39:59 -0500

    Add .gitignore and format colours file.

--- led_colour.py
+++ led_colour.py
@@ -4,18 +4,22 @@ class Colour:
     self.g = g
     self.b = b
     self.name = name
-  
+
   def add(self, other):
     return Colour(self.r + other.r, self.g + other.g, self.b + other.b)
-  
+
   def multiply(self, factor):
-    return Colour(int(self.r * factor), int(self.g * factor), int(self.b * factor))
-  
+    return Colour(
+        int(self.r * factor),
+        int(self.g * factor),
+        int(self.b * factor))
+
   def __str__(self):
     if self.name:
       return self.name
     else:
       return "({0}, {1}, {2})".format(self.r, self.g, self.b)
+
 
 Colour.WHITE = Colour(255, 255, 125, "White")
 Colour.RED = Colour(150, 0, 0, "Red")
