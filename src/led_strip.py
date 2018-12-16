2c2ccc04fcf6a3dad6c0b364dce4d46e0f547d63
Leo Denault <leo.denault@gmail.com>
2018-12-15 20:41:19 -0500

    Reorganize the LedStrip class for clarity.

--- src/led_strip.py
+++ src/led_strip.py
@@ -1,35 +1,55 @@
-"""Playground library for experimenting with WS2801 LEDs.
-
-This module is meant to be imported when experimenting on the python CLI with
-an LED strip.
-"""
-
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_WS2801
 
-_ALL_LEDS = -1
+
+class LedDirection:
+    """Enum class for defining directions to output light patterns on an LED
+    strip connected to a Raspberry Pi.
+    """
+
+    START_TO_END = 0
+    """Output patterns so that 
+        the first pixel in the pattern matches with the first physical LED on 
+        the strip.
+    """
+    END_TO_START = 1
+    """Output patterns so that 
+        the first pixel in the pattern matches with the last physical LED on 
+        the strip
+    """
 
 
-class Playground:
-    def __init__(self, pixel_count, start_to_end, spi_port=0, spi_device=0):
-        """Creates a playground object for experimenting with WS2801 LEDs.
+class LedStrip:
+    """Abstraction around a WS2801 LED strip connected to a Raspberry Pi
+    through its GPIO pins and communicating through SPI.
+    """
 
-        :param pixel_count: the number of pixels to output to. This is
-        usually the total number of LEDs on the strip.
-        :param start_to_end: True if index 0 should correspond to the first
-        LED on the strip, False if it should correspond to the last LED on
-        the strip (with index 1 corresponding to the next-to-last LED,
-        and so on).
+    _ALL_LEDS = -1
+
+    def __init__(
+      self,
+      num_leds,
+      direction=LedDirection.START_TO_END,
+      spi_port=0,
+      spi_device=0):
+        """Creates an LedStrip object for communicating with an LED strip 
+        connected to the Raspberry Pi.
+        
+        :param num_leds: the number of LEDs on the strip.
+        :param direction: the direction in which animations should be 
+        rendered. Use LedDirection.START_TO_END to output patterns so that 
+        the first pixel in the pattern matches with the first physical LED on 
+        the strip. Use LedDirection.END_TO_START to output patterns so that 
+        the first pixel in the pattern matches the last physical LED on the 
+        strip.
         :param spi_port: the SPI port to output to. Defaults to 0.
-        :param spi_device: the SPI device to output to. For example, a value
-        of 0 would output to /dev/spidev0.0, a value of 1 outputs to
-        /dev/spidev0.1. Defaults to 0.
+        :param spi_device: the SPI device to output to. For example, 
+        specifying a device of 1 will use /dev/spidev0.1. Defaults to 0.
         """
-
-        self.pixel_count = pixel_count
-        self.start_to_end = start_to_end
-        self.pixels = Adafruit_WS2801.WS2801Pixels(
-          pixel_count, spi=SPI.SpiDev(spi_port, spi_device))
+        self.leds = Adafruit_WS2801.WS2801Pixels(
+          num_leds, spi=SPI.SpiDev(spi_port, spi_device))
+        self.direction = direction
+        self.num_leds = num_leds
 
     def display(self, colour, led=_ALL_LEDS):
         """Displays a specific colour on a specific LED.
@@ -39,17 +59,17 @@ class Playground:
         unspecified, then colour is displayed on all LEDs.
         """
 
-        if led == _ALL_LEDS:
-            for i in range(0, self.pixel_count):
-                self.pixels.set_pixel_rgb(i, colour.r, colour.g, colour.b)
+        if led == LedStrip._ALL_LEDS:
+            for i in range(0, self.num_leds):
+                self.leds.set_pixel_rgb(i, colour.r, colour.g, colour.b)
         else:
-            led = led if self.start_to_end else self.pixel_count - led - 1
-            self.pixels.set_pixel_rgb(led, colour.r, colour.g, colour.b)
-        self.pixels.show()
+            led = led if self.direction else self.num_leds - led - 1
+            self.leds.set_pixel_rgb(led, colour.r, colour.g, colour.b)
+        self.leds.show()
 
     def clear(self):
         """Clears the LED strip of any colours currently displayed.
         """
 
-        self.pixels.clear()
-        self.pixels.show()
+        self.leds.clear()
+        self.leds.show()
