48265c01a0a61b533fd6fee95ffce0b3f7b18fac
Leo Denault <leo.denault@gmail.com>
2019-01-12 19:40:50 -0500

    Create Tkinter widget as part of providing a softare LED strip device.

---
+++ src/display/tkinter/led_widget.py
@@ -0,0 +1,36 @@
+from Tkinter import Frame
+
+
+class LedWidget:
+    """Tk widget representing an LED on a strip.
+    """
+
+    def __init__(self, master, width, height):
+        """Instantiates an LED widget.
+
+        :param master: the parent container of this widget.
+        :param width: the width of this widget.
+        :param height: the height of thie widget.
+        """
+        self.widget = Frame(master)
+        self.widget["bg"] = "white"
+        self.widget["width"] = width
+        self.widget["height"] = height
+        self.widget["borderwidth"] = 1
+        self.widget["relief"] = "solid"
+
+    def pack(self, options):
+        """Displays and arranges, geometrically, this widget.
+
+        :param options: the Tk options to use when packing this widget.
+        """
+        self.widget.pack(options)
+
+    def set_led_colour(self, colour):
+        """Sets the colour of the LED widget to the given colour.
+        """
+        self.widget["bg"] = self._convert_colour_to_hex(colour)
+
+    def _convert_colour_to_hex(self, colour):
+        return "#" + str(hex(colour.r)) + str(hex(colour.g)) + str(
+          hex(colour.b))
