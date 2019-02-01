bc8bf4eb5a1154bdddad652dba5e3348eeb67979
Leo Denault <leo.denault@gmail.com>
2019-01-31 23:52:21 -0500

    Fix bug in streaming pattern where LEDs would be drawn to more than once.

--- src/pattern/stream_pattern.py
+++ src/pattern/stream_pattern.py
@@ -28,15 +28,15 @@ class StreamPattern(TimedPattern):
     def __init__(
       self,
       num_leds,
-      leds_per_second,
+      period,
       interpolation_mode,
       colour_palette,
       display_time):
         """Creates a StreamPattern object.
 
         :param num_leds: the number of LEDs on the device.
-        :param leds_per_second: the amount, in LED pixels, by which to move
-        the position of the colours every second
+        :param period: the amount of time for a colour to wrap around the LED
+        strip.
         :param interpolation_mode: the InterpolationMode to use to
         interpolate between two colours on the LED strip.
         :param colour_palette: the set of colours which should be used to
@@ -45,32 +45,32 @@ class StreamPattern(TimedPattern):
         pattern.
         """
         super(StreamPattern, self).__init__(display_time)
-        self.leds_per_second = leds_per_second
+        self.period = period
         self.colour_palette = colour_palette
         self.interpolation_mode = interpolation_mode
         self.segment_length = (
           float(num_leds) / len(self.colour_palette))
-        self.colour_positions = [i * self.segment_length for i in
-                                 range(0, len(colour_palette))]
 
     def update(self, leds, delta):
         super(StreamPattern, self).update(leds, delta)
-        led_offset = delta * self.leds_per_second
-        self.colour_positions = [
-            (led_offset + self.colour_positions[i]) % leds.get_num_leds() for i
-            in range(0, len(self.colour_positions))]
+        num_leds = leds.get_num_leds()
+        led_offset = (
+          ((self.elapsed_time % self.period) / self.period) * num_leds)
+        num_colours = len(self.colour_palette)
 
-        for i in range(0, len(self.colour_palette)):
-            colour_index = self.colour_positions[i]
-            for j in range(0, int(self.segment_length)):
+        for i in range(0, num_colours):
+            colour_position = (i * self.segment_length + led_offset) % num_leds
+            j = 0
+            while math.ceil(
+              colour_position + j) < colour_position + self.segment_length:
                 intermediate_led_index = int(math.ceil(
-                  (colour_index + j)) % leds.get_num_leds())
-                distance = intermediate_led_index - colour_index
+                  (colour_position + j)) % num_leds)
+                distance = intermediate_led_index - colour_position
                 if distance < 0:
-                    distance = leds.get_num_leds() + distance
+                    distance += num_leds
                 interpolated_colour = self.interpolation_mode.interpolate(
                   self.colour_palette[i],
-                  self.colour_palette[(i + 1) % len(self.colour_palette)],
+                  self.colour_palette[(i + 1) % num_colours],
                   1 - (distance / self.segment_length))
                 leds.set_colour(interpolated_colour, intermediate_led_index)
-        leds.display()
+                j += 1
